469b5a58ee39a8184728c5da6e5ce11e3e29fa68
Tabrez Dal <tabrez.dal@gmail.com>
2023-09-02 15:24:47 +0530

    Scripts moved to Security Folder from Index.html
    To successfully auto Deploy with gitHub Actions

---
+++ src/Security/preventContextMenu.js
@@ -0,0 +1,25 @@
+const preventContextMenu = () => {
+  document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+  });
+
+  document.onkeydown = (e) => {
+    if (e.key === "F12") {
+      e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "I") {
+      e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "C") {
+      e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key === "J") {
+      e.preventDefault();
+    }
+    if (e.ctrlKey && e.key === "U") {
+      e.preventDefault();
+    }
+  };
+};
+
+export default preventContextMenu;
