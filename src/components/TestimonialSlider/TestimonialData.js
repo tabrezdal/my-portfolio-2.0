f6a73d889010f1d95e152b9bf608918523e00730
Tabrez Dal <tabrez.dal@gmail.com>
2026-01-29 16:30:19 +0530

    Testimonials Updated

--- src/components/TestimonialSlider/TestimonialData.js
+++ src/components/TestimonialSlider/TestimonialData.js
@@ -1,7 +1,16 @@
 const TestimonialData = [
   {
     avatar:
-      "https://media.licdn.com/dms/image/D4D03AQFtj0TSNT3j8Q/profile-displayphoto-shrink_100_100/0/1688669159050?e=1720656000&v=beta&t=_pNKo620KSjCse25Sv2NrItEJLCkMPpkRxeSPaQ72Lw",
+      "https://media.licdn.com/dms/image/v2/D5635AQGcRQd4XNaoZw/profile-framedphoto-shrink_400_400/B56ZvUZ8.xJ8Ak-/0/1768795132059?e=1770289200&v=beta&t=DLs7g4YKQIxzdNO79GvD--KpBLkYmafNxFgKiOaqclc",
+    name: "Akshay Shetty",
+    review:
+      "We hired Tabrez for a complete UI/UX redesign of our platform, and he exceeded expectations. His exceptional design knowledge and technical expertise created a visually striking yet highly functional interface. Designs and prototypes were not only stunning but also user-friendly, ensuring an intuitive experience across the entire application. He built a comprehensive design system that streamlined our workflow. his ability to blend design with technical requirements, while aligning with our business goals, was invaluable. We’re extremely pleased and highly recommend him for any UI/UX project.",
+    sourceUrl:
+      "https://www.upwork.com/freelancers/tabrezdal",
+  },
+  {
+    avatar:
+      "https://media.licdn.com/dms/image/v2/D5635AQGfOQvoHgMDIw/profile-framedphoto-shrink_400_400/B56Zs7ER7lIYAc-/0/1766222536670?e=1770289200&v=beta&t=3-a8Gl9WgZMmCR4aG5iwp0OCbuUFGw5zfxPKSBAe51E",
     name: "Shahzaib Arshad",
     review:
       "What sets Tabrez apart is his remarkable creativity and attention to detail. He consistently approached each design challenge with a fresh perspective, offering innovative solutions that elevated the overall user experience. His designs not only met the functional requirements of the project but also added a layer of aesthetic refinement that resonated with our target audience. Tabrez's collaboration skills were equally notable. He seamlessly integrated feedback and iterated on his designs, always prioritizing the project's goals while remaining open to suggestions. His professionalism and effective communication made him a pleasure to work with, fostering a positive and productive environment within our team. I'd highly recommend Tabrez for any UI/UX project",
@@ -10,7 +19,7 @@ const TestimonialData = [
   },
   {
     avatar:
-      "https://media.licdn.com/dms/image/D4E03AQEwp6ei8NSelw/profile-displayphoto-shrink_100_100/0/1707323308566?e=1720656000&v=beta&t=1g-9bxkpwt_Z7ztai5LDdHY1eylVwXwPyIBzMyn-G2o",
+      "https://media.licdn.com/dms/image/v2/D4E03AQEwp6ei8NSelw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1707323308566?e=1771459200&v=beta&t=FPonxpOC4ZlRrethXHyRJsL96GdzqUfMXNQrwIDzldI",
     name: "Malvika Singh",
     review:
       "Working with Tabrez Dal on my Apparel Catalog Design and Ui/Ux project was an absolute delight. Tabrez's keen eye for detail and creativity shone through in every aspect of the project. From the intricate flat sketches and technical sketches for the catalog to his thorough exploration of user journeys, personas, and the entire Ui/Ux process, Tabrez showcased a level of dedication and skill that sets him apart. The end results were not just designs; they were a perfect representation of my vision brought to life. Highly recommended!",
@@ -19,7 +28,24 @@ const TestimonialData = [
   },
   {
     avatar:
-      "https://media.licdn.com/dms/image/C5103AQFP2uiAMnCEog/profile-displayphoto-shrink_100_100/0/1580806984740?e=1720656000&v=beta&t=5veRrSPV6RD6cE1LgSS4eRBzwGcNSuwzM4qlTkFteGw",
+      "https://media.licdn.com/dms/image/D4E03AQEFwVYLby9YvA/profile-displayphoto-shrink_200_200/0/1685436020851?e=2147483647&v=beta&t=6-Rkq-j_zGUUwT56PbRoOCJ-tRFfWTEykNR2ZVWgxm4",
+    name: "Zak Furness",
+    review: "I have to be honest when I leave reviews even if the freelancer does a good job. He is a good designer but if he promises to drop the attitude he is a great designer.",
+    sourceUrl:
+      "https://www.linkedin.com/in/tabrezdal/details/recommendations/?detailScreenTabIndex=0",
+  },
+  {
+    avatar:
+      "https://media.licdn.com/dms/image/v2/D5635AQGcRQd4XNaoZw/profile-framedphoto-shrink_400_400/B56ZvUZ8.xJ8Ak-/0/1768795132059?e=1770289200&v=beta&t=DLs7g4YKQIxzdNO79GvD--KpBLkYmafNxFgKiOaqclc",
+    name: "Akshay Shetty",
+    review:
+      "Tabrez is our go-to choice for a landing page project, and he once again proved his expertise in UI/UX. His creativity and meticulous attention to detail shone through, resulting in a design that was not only on-brand but also user-focused, effectively driving engagement and conversions. Despite a tight deadline, Tabrez delivered a high-quality result with remarkable attention to detail. His communication and professionalism throughout the project were excellent. We will definitely be hiring him for future projects!",
+    sourceUrl:
+      "https://www.upwork.com/freelancers/tabrezdal",
+  },
+  {
+    avatar:
+      "https://media.licdn.com/dms/image/v2/C5103AQFP2uiAMnCEog/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1580806984766?e=1771459200&v=beta&t=0fNivxpgKk5-vpMd4RGNaTkQFPPtdABdLLzUvv9CWMk",
     name: "Viral Patel",
     review:
       "Working with Tabrez Dal has been an exceptional experience. His creativity, professionalism, and attention to detail truly set him apart. I was thoroughly impressed with the results he delivered on our project. The complimentary banner edits were a generous gesture for being Loyal Customer, and the two additional variants he provided showcased his commitment to excellence. He exceeded our expectations, and I wholeheartedly recommend him to anyone in need of top-tier design services. Looking forward to future collaborations!",
@@ -28,7 +54,7 @@ const TestimonialData = [
   },
   {
     avatar:
-      "https://media.licdn.com/dms/image/D5603AQHWZMUayQUfrA/profile-displayphoto-shrink_100_100/0/1675251692358?e=1720656000&v=beta&t=JJhW8x5exaapc6Q390jFl4if5WumWxWWPPBViI0F6A4",
+      "https://media.licdn.com/dms/image/v2/D5603AQHWZMUayQUfrA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1675251692358?e=1771459200&v=beta&t=4ktwRWUcNynCFtdXALSQlUgdIYPrI5GgqwALghLop5A",
     name: "Mohammed Omer Farooq",
     review:
       "I find Tabrez has one unique sense of creativity in his work and I feel he is the more creative person which makes him better for Web Developer and Graphic Design professional. Communication is polite very humble person person to talk with.",
@@ -37,7 +63,7 @@ const TestimonialData = [
   },
   {
     avatar:
-      "https://media.licdn.com/dms/image/D4D03AQElgtpz9r0SAg/profile-displayphoto-shrink_100_100/0/1673880834197?e=1720656000&v=beta&t=IaTrYfbMeeWTWs3FpQd_ysLJiQHsj8GIebKRDghHhHw",
+      "https://media.licdn.com/dms/image/v2/D4D03AQElgtpz9r0SAg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1673880834197?e=1771459200&v=beta&t=xKD6xi0biCZNalwoOBceIWQD7M2ZYpBQkQl9OeatyZw",
     name: "Lee-Anthony Pinn",
     review:
       "This is the first time I have reached out to someone to work on a project for me and Tabrez really has exceeded all expectations. Quality work and easy to work with.",
@@ -46,18 +72,10 @@ const TestimonialData = [
   },
   {
     avatar:
-      "https://media.licdn.com/dms/image/C4E03AQEH_rP45WNdKQ/profile-displayphoto-shrink_100_100/0/1592461133539?e=1720656000&v=beta&t=u36uFCC52gMhUGfc-fKWxODj8VeC-K_WoYZSOZVtVqM",
+      "https://media.licdn.com/dms/image/v2/C4E03AQEH_rP45WNdKQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1592461133540?e=1771459200&v=beta&t=IyauOaa2TJqebyUnUH93PN0W07yk-J06YahU-qNa1T0",
     name: "Kishan Majeethiya",
     review:
       "Tabrez Dal's Design sense exceeded all my expectations. The designs he delivered for my brands were not only visually stunning but also resonated perfectly with my brand identity. What sets Tabrez apart is not just his design acumen but also his commitment to client satisfaction. I was not only impressed enough to become a returning client but also confidently referred him to friends seeking a professional of his caliber. Tabrez's work speaks for itself, and I couldn't be happier with the results.",
-    sourceUrl:
-      "https://www.linkedin.com/in/tabrezdal/details/recommendations/?detailScreenTabIndex=0",
-  },
-  {
-    avatar:
-      "https://media.licdn.com/dms/image/D4E03AQEFwVYLby9YvA/profile-displayphoto-shrink_200_200/0/1685436020851?e=2147483647&v=beta&t=6-Rkq-j_zGUUwT56PbRoOCJ-tRFfWTEykNR2ZVWgxm4",
-    name: "Zak Furness",
-    review: "I have to be honest when I leave reviews even if the freelancer does a good job. He is a good designer but if he promises to drop the attitude he is a great designer.",
     sourceUrl:
       "https://www.linkedin.com/in/tabrezdal/details/recommendations/?detailScreenTabIndex=0",
   },
